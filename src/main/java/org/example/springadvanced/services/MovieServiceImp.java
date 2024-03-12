ac3d6b91a92c6cffeeab42e6d983ae23cd75ccd4
Porostovo <petittcheque@gmail.com>
2024-03-12 18:24:07 +0100

    adding safe to database feature

--- src/main/java/org/example/springadvanced/services/MovieServiceImp.java
+++ src/main/java/org/example/springadvanced/services/MovieServiceImp.java
@@ -1,7 +1,12 @@
 package org.example.springadvanced.services;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.example.springadvanced.models.Movie;
+import org.example.springadvanced.repositories.MovieRepository;
 import org.example.springadvanced.util.RetrofitUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import retrofit2.Call;
@@ -16,12 +21,17 @@ import java.util.List;
 
 @Service
 @Component
-public class MovieServiceImp implements MovieService{
+public class MovieServiceImp implements MovieService {
 
     private Retrofit retrofit;
     private MovieApi movieApi;
+    private MovieRepository movieRepository;
 
-    public MovieServiceImp() {
+    @Autowired
+    public MovieServiceImp(MovieRepository movieRepository) {
+
+        this.movieRepository = movieRepository;
+
         retrofit = RetrofitUtil.getRetrofitInstance();
         movieApi = retrofit.create(MovieApi.class);
     }
@@ -33,8 +43,8 @@ public class MovieServiceImp implements MovieService{
 
 
     @Override
-    public void getMovies( Callback<List<Movie>> callback){
-       Call<List<Movie>> getMovies = movieApi.getMovies("Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjI4MzhkZGQ4ZDFjMWFlZmUwODFmMzdiYzc3NzE3MCIsInN1YiI6IjY1ZjA0ZWIwN2YwNTQwMDE2NDg1ZDZmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2sO58bPqK8B43OdbVKhc8Widfo_Bm-FuEXWrKtQKok0");
+    public void getMovies(Callback<List<Movie>> callback) {
+        Call<List<Movie>> getMovies = movieApi.getMovies("Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjI4MzhkZGQ4ZDFjMWFlZmUwODFmMzdiYzc3NzE3MCIsInN1YiI6IjY1ZjA0ZWIwN2YwNTQwMDE2NDg1ZDZmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2sO58bPqK8B43OdbVKhc8Widfo_Bm-FuEXWrKtQKok0");
 //       try {
 //           Response<List<Movie>> response = getMovies.execute();
 //           if (response.isSuccessful() && response.body() != null) {
@@ -43,7 +53,7 @@ public class MovieServiceImp implements MovieService{
 //       } catch (Exception e) {
 //           throw new RuntimeException(e)   }
 //       return movieList;
- //  }
+        //  }
 
         getMovies.enqueue(new Callback<List<Movie>>() {
             @Override
@@ -54,6 +64,7 @@ public class MovieServiceImp implements MovieService{
 
                 }
             }
+
             @Override
             public void onFailure(Call<List<Movie>> call, Throwable t) {
                 callback.onFailure(call, t);
@@ -62,7 +73,8 @@ public class MovieServiceImp implements MovieService{
     }
 
     @Override
-    public void getListOfMoviesAndSave() throws IOException {
+    public Object getListOfMoviesAndSave() throws IOException {
+
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("https://api.themoviedb.org/3/")
@@ -75,7 +87,20 @@ public class MovieServiceImp implements MovieService{
                         "N1YiI6IjY1ZjA0ZWIwN2YwNTQwMDE2NDg1ZDZmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ" +
                         ".2sO58bPqK8B43OdbVKhc8Widfo_Bm-FuEXWrKtQKok0");
 
+        //take original title from recieved JSON format
         Object response = call.execute().body();
-        System.out.println(response);
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(response);
+        JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
+        JsonArray resultsArray = jsonObject.getAsJsonArray("results");
+
+        for (int i = 0; i < resultsArray.size(); i++) {
+            Movie movie = new Movie();
+            JsonObject resultObject = resultsArray.get(i).getAsJsonObject();
+            String original_title = resultObject.get("original_title").getAsString();
+            movie.setOriginal_title(original_title);
+            movieRepository.save(movie);
+        }
+        return response;
     }
 }
